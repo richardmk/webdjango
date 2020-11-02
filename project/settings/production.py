1d9be5725166ac162f6cf6774ff752349eeb6b17
Richard <rmejia@yarnoldconsultores.pe>
2020-11-02 16:03:57 -0500

    static

--- project/settings/production.py
+++ project/settings/production.py
@@ -19,7 +19,7 @@ STATICFILES_DIRS = (
 )
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_STORAGE = STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
