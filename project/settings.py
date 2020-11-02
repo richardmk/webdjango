e4a4fb09bb2be440390f4bf5c0d86b13d41dccf5
Richard <rmejia@yarnoldconsultores.pe>
2020-11-02 15:16:02 -0500

    staticfiles_dirs delete

--- project/settings.py
+++ project/settings.py
@@ -140,9 +140,9 @@ EMAIL_HOST_PASSWORD = "Georgeana10"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
 
 MEDIA_ROOT = '/media/'
 
