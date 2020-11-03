1154c999ed08a2dbc492f349d66e03bb37b950e8
Richard <rmejia@yarnoldconsultores.pe>
2020-11-03 11:48:00 -0500

    deploy

--- project/settings/production.py
+++ project/settings/production.py
@@ -1,23 +1,2 @@
 from .base import *
 
-DEBUG = False
-ALLOWED_HOSTS = ['*']
-
-import dj_database_url
-from decouple import config
-DATABASES = {
-    'default' : dj_database_url.config(
-        default = config('DATABASE_URL')
-    )
-}
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
