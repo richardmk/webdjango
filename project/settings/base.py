be3925a9ff7d1b0aaa78d6435a93afa7aa97e717
Richard <rmejia@yarnoldconsultores.pe>
2020-11-03 21:13:33 -0500

    deploy

--- project/settings/base.py
+++ project/settings/base.py
@@ -79,6 +79,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "richard.mejia.k@gmail.com"
-EMAIL_HOST_PASSWORD = "Georgeana10" 
+EMAIL_HOST_USER = "######"
+EMAIL_HOST_PASSWORD = "#####" 
 
